1f1486962c90269d8ab8adca9960abcf6d9329a5
Golub Golubovic <golubgolubovic@yandex.com>
2025-01-19 19:35:26 +0100

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-<script>
-//Refresh Captcha
-function refreshCaptcha()
-     img = document.images['captcha_image'];
-    img.src = img.src.substring(
-		0,img.src.lastIndexOf("?")
-		)+"?rand="+Math.random()*1000;
-
-</script>
